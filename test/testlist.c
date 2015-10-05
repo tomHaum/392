19062efcbc7e3fe88e19a94d696dc00cba51a1c9
Tom Haumersen <tbhizzle@yahoo.com>
2015-10-05 16:27:28 -0400

    Adds header and testlist.c

---
+++ test/testlist.c
@@ -0,0 +1,122 @@
+#include "../include/my.h"
+#include "../include/mylist.h"
+#include <stdlib.h>
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+/*
+  while(*head = (*head)->next)
+  free((*head)->prev);
+  free(*head);
+ */
+int main(int argc, char const *argv[]){
+  struct s_node** myhead = (struct s_node**)malloc(sizeof(struct s_node**));
+  struct s_node* nodeptr;
+
+  int mynum;
+  int *mynumptr;
+  int mynum2;
+  int *mynumptr2;
+  int mynum3;
+  int *mynumptr3;
+  int mynum4;
+  int *mynumptr4;
+  int mynum5;
+  int *mynumptr5;
+  int mynum6;
+  int *mynumptr6;
+  mynum = 4;
+  mynumptr = &mynum;
+  mynum2 = 5;
+  mynumptr2 = &mynum2;
+  mynum3 = 7;
+  mynumptr3 = &mynum3;
+  mynum4 = 8;
+  mynumptr4 = &mynum4;
+  mynum5 = 9;
+  mynumptr5 = &mynum5;
+  mynum6 = 10;
+  mynumptr6 = &mynum6;
+  my_str("Length of the list(should be 0): ");
+  my_int(count_s_nodes(*myhead));
+  my_char('\n');
+
+  *myhead = new_node(mynumptr2,0,0);
+  nodeptr = *myhead;
+
+  add_elem(&mynum,myhead);
+  my_str("add 4 to front: ");
+  traverse_int(*myhead);
+  my_char('\n');
+
+  nodeptr = new_node((void *)mynumptr3,0,0);
+  my_str("testing print_int");
+  print_int(nodeptr);
+  my_char('\n');
+
+  my_str("new list(7 appended): ");
+  append(nodeptr,myhead);
+  traverse_int(*myhead);
+  my_char('\n');
+
+  nodeptr = new_node((void *)mynumptr4,0,0);
+  add_node(nodeptr,myhead);
+  my_str("Added 8 to front: ");
+  traverse_int(*myhead);
+  my_char('\n');
+
+  nodeptr = new_node((void *)mynumptr5,0,0);
+  add_node_at(nodeptr,myhead,1);
+  my_str("Added 9 to index 1: ");
+  traverse_int(*myhead);
+  my_char('\n');
+
+  nodeptr = new_node((void *)mynumptr6,0,0);
+  add_node_at(nodeptr,myhead,0);
+  my_str("Added 10 to index 0: ");
+  traverse_int(*myhead);
+  my_char('\n');
+
+  my_str("Length of the list(should be 6): ");
+  my_int(count_s_nodes(*myhead));
+  my_char('\n');
+
+  debug_int(*myhead);
+  my_char('\n');
+  my_char('\n');
+
+  my_str("Remove first node(10)\n");
+  remove_node(myhead);
+  traverse_int(*myhead);
+  my_char('\n');
+  my_char('\n');
+
+  my_str("adding 10 back to front\n");
+  add_node(nodeptr,myhead);
+  traverse_int(*myhead);
+  my_char('\n');
+  my_char('\n');
+
+  my_str("remove second elem\n");
+  my_int(*((int *) remove_node_at(myhead,1)));
+  my_char('\n');
+  traverse_int(*myhead);
+  my_char('\n');
+
+  my_str("remove last elem\n");
+  my_int(*((int *) remove_last(myhead)));
+  my_char('\n');
+  traverse_int(*myhead);
+  my_char('\n');
+
+  while((*myhead)->next){
+    remove_node_at(myhead, 1);
+  }
+  my_str("\nRemoved all but head\n");
+  traverse_int(*myhead);
+  my_char('\n');
+
+
+
+}
+
